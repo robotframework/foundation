f8bbec0dccec113fd0b0166dbb60f119b0a638af
Marko Klemetti <marko.klemetti@gmail.com>
2016-03-03 16:43:59 +0200

    Add first version of foundation web pages

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  var elementHeight = $('.aboutTextContainer').height();
+
+  $('#aboutImage').css('height', elementHeight);
+
+  $(window).resize(function() {
+    var elementHeight = $('.aboutTextContainer').height();
+    $('#aboutImage').css('height', elementHeight);
+  });
+
+  $('.bxslider').bxSlider({
+    pagerCustom: '#bx-pager',
+    mode: 'fade',
+    controls: false
+  });
+  
+  $('nav a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing');
+  });
+});
