44d50701d0e7685b3f4a7e29973f3245daf45cbf
Sami Kolari <sami.kolari@siilim180518.local>
2019-12-12 12:34:58 +0200

    fix privacypolicy link

--- sources/src/components/AppFooter.vue
+++ sources/src/components/AppFooter.vue
@@ -4,7 +4,7 @@
           <h2 style="text-align:center;" class="row-header">Contact info</h2>
           <a style="text-align:center; color: white; width: 100%; display: block" href="mailto:robotframework-foundation@googlegroups.com">
                   robotframework-foundation@googlegroups.com</a>
-            <a style="position: absolute; right: 1rem; color: #eee;" href="https://robotframework.org/#/privacypolicy" target="_blank">Privacy Policy</a>
+            <a style="position: absolute; right: 1rem; color: #eee;" href="https://robotframework.org/privacypolicy" target="_blank">Privacy Policy</a>
     </footer>
 
 
