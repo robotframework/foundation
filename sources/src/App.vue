2fb898f878d0042ad2901ae010d3af1734441e2d
Sami Kolari <sami.kolari@siilim180518.local>
2020-03-20 15:23:49 +0200

    added sleepiq

--- sources/src/App.vue
+++ sources/src/App.vue
@@ -314,6 +314,13 @@ export default {
                   "We are a modern, full-service technology company. We help you create better business and meaningful digital experiences by combining strategic insight, creative design, data management, and technology expertise."
               },
               {
+                title: "sleepIQ",
+                href: "https://bamlabs.com/",
+                img: require("@/assets/img/users/sleepiq.png"),
+                text:
+                  "SleepIQ Labs is a subsidiary of Sleep Number Corporation, the creators of the Sleep Number bed. SleepIQ Labs, the pioneers of biometric sensor sleep solutions and sleep-monitoring technologies, was acquired by Sleep Number in 2015. SleepIQ has been foundational in elevating Sleep Number’s sleep innovation leadership and is quickly transforming the Sleep Number 360® smart bed into the hub of health and wellness."
+              },
+              {
                 title: "symbio",
                 href: "http://www.symbio.com/",
                 img: require("@/assets/img/users/symbio.jpg"),
