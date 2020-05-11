e6bd22c16961f43fffd74a9a75f2202eaccd5aad
Sami Kolari <sami.kolari@siilim180518.local>
2020-05-11 20:49:34 +0300

    updated price table

--- sources/src/components/TextBlock.vue
+++ sources/src/components/TextBlock.vue
@@ -6,14 +6,22 @@
     </div>
     <div class="col order-2 textblock-right ml-1 p-3">
       <div v-html="data.text"/>
+      <div v-if="data.priceTable == true" class="row order-3">
+        <div class="col-sm-12 mt-3">
+          <b>Annual Fees</b>
+        </div>
+        <div class="col-sm-12">
+          Annual fee is determined by how many Robot Framework users are in your organization.
+        </div>
+        <div class="col-sm-12 col-md-10">
+          <b-table striped hover class="mt-2" :items="items" :fields="fields"></b-table>
+        </div>
+      </div>
     </div>
 
      <div v-if="data.twitter == true" class="col-md-12 col-lg-3 order-3 textblock-right ml-1 p-3">
        <twitter-timeline class="p-xs-5 p-lg-0" id="robotframework" widget-class="twitterprofile" sourceType="profile" :options="options"/>
      </div>
-     </div>
-     <div v-if="data.priceTable == true" class="row order-3 ml-sm-1 justify-content-end col-md-8 float-right">
-       <b-table striped hover :items="items" :fields="fields"></b-table>
      </div>
      <div v-if="typeof data.textAfterTable == 'string'" class="row justify-content-end float-right">
       <div v-html="data.textAfterTable" class="ml-3 col-sm-12 col-md-8 order-3"/>
@@ -31,35 +39,30 @@ export default {
   },
   data() {
     return {
-      fields: ["number_of_users", "service_providers", "product_companies"],
+      fields: ["users", "fee"],
       items: [
         {
           isActive: true,
-          age: 40,
-          number_of_users: "1-10",
-          service_providers: "1000€",
-          product_companies: "500€"
+          users: "1-10",
+          fee: "1000€"
         },
         {
           isActive: false,
           age: 21,
-          number_of_users: "11-50",
-          service_providers: "2000€",
-          product_companies: "1000€"
+          users: "11-50",
+          fee: "2000€"
         },
         {
           isActive: false,
           age: 89,
-          number_of_users: "51-250",
-          service_providers: "4000€",
-          product_companies: "2000€"
+          users: "51-250",
+          fee: "4000€"
         },
         {
           isActive: true,
           age: 38,
-          number_of_users: "251-",
-          service_providers: "8000€",
-          product_companies: "4000€"
+          users: "251-",
+          fee: "8000€"
         }
       ],
       options: {
