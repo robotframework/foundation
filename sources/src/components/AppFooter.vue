4d20f9a7b4b7e93aaf39fc8ad7113727faee02cf
Sami Kolari <sami.kolari@siilim180518.local>
2021-06-03 17:29:52 +0300

    contact details

--- sources/src/components/AppFooter.vue
+++ sources/src/components/AppFooter.vue
@@ -2,8 +2,20 @@
  <!-- Footer -->
     <footer>
           <h2 style="text-align:center;" class="row-header">Contact info</h2>
+          <div class="text-center text-white">
+            <div>
+              Robot Framework ry
+            </div>
+            <div>
+              Kampinkuja 2
+            </div>
+            <div>
+              00100 Helsinki, Finland
+            </div>
+          </div>
           <a style="text-align:center; color: white; width: 100%; display: block" href="mailto:robotframework-foundation@googlegroups.com">
-                  robotframework-foundation@googlegroups.com</a>
+            robotframework-foundation@googlegroups.com
+          </a>
             <a style="position: absolute; right: 1rem; color: #eee;" href="https://robotframework.org/privacypolicy" target="_blank">Privacy Policy</a>
     </footer>
 
